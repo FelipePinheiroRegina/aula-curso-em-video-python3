41783490ddc935e38db7214f6364b11a401b4e0d
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-04-23 18:38:49 -0300

    aula 15, while true, break

---
+++ aula015.py
@@ -0,0 +1,16 @@
+#while True:
+ #   print('Olá, Mundo!')
+  #  break
+'''s = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s = s + n
+print(f'A soma dos números digitados é {s}')'''
+nome = 'José'
+idade = 33
+salario = 997.3
+print(f'O {nome} tem {idade} anos')
+print('O {} tem {} anos'.format(nome, idade))
+print(f'O {nome} tem {idade} anos e ganha {salario:.2f}')

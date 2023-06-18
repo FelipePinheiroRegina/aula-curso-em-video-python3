b40cab06ebecc90692090253a2aebc83979d0daf
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-06-18 15:48:10 -0300

    aula020

---
+++ aula020.py
@@ -0,0 +1,39 @@
+'''def mostralinha():
+    print('-' * 30)
+
+# Programa Principal
+nomecompleto = {'nome' : str(input('Primeiro nome: ')),
+                'sobrenome' : str(input('Sobrenome: ')),
+                'ultimonome' : str(input('Ultimo nome: '))}
+
+mostralinha()
+print(f"{nomecompleto['nome']}")
+mostralinha()
+print(f"{nomecompleto['sobrenome']}")
+mostralinha()
+print(f"{nomecompleto['ultimonome']}")
+mostralinha()
+'''
+
+
+'''def titulo(msg):
+    print('-' * 20)
+    print(msg)
+    print('-' * 20)
+
+
+# Programa Principal
+nome = str(input('Nome: '))
+titulo(f'  Olá {nome}')'''
+
+
+'''def soma(x, y):
+    print('=-' * 10)
+    print(f'{x} + {y} = {x + y}')
+    print('=-' * 10)
+
+
+# Programa Principal
+a = int(input('Entre com um número: '))
+b = int(input('Entre com outro número: '))
+soma(a, b)
